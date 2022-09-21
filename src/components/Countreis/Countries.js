a4d40159a3a0ae3049baee42043d4accae1638ee
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-21 20:34:09 +0600

    added key

--- src/components/Countreis/Countries.js
+++ src/components/Countreis/Countries.js
@@ -15,8 +15,10 @@ const Countries = () => {
       <div>
         <h1>Welcome our Countries.. {countries.length}</h1>
         <div className="countries-container">
-          {countries.map((country) => (
-            <Country country={country}></Country>
+          {countries.map((country) => (<Country 
+            country={country}
+            key={country.cca3}
+            ></Country>
           ))}
         </div>
       </div>
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Country.css';
 const Country = (props) => {
-    console.log(props)
-    const {area, name, population, languages} = props.country
+    // console.log(props.country)
+    const {area, name, population, languages, flags} = props.country
     return (
       <div className="country">
         <h3>{name.common}</h3>
+        <img src={flags.png} alt="" />
         <p>Area: {area}</p>
         <p>Population: {population}</p>
         <p>Language: {languages?.bul}</p>
